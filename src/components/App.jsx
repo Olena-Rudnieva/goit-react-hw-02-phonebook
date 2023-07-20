098eb170b7812ab93bc7d241670685e72d628e06
Olena-Rudnieva <rudnievao@gmail.com>
2023-07-20 13:24:18 +0300

    Add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <form action="">
+          <label htmlFor="">
+            Name
+            <input type="text" />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
