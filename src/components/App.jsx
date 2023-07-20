ecb4397b6328519268bfee6c58a62df27037ea63
Olena-Rudnieva <rudnievao@gmail.com>
2023-07-20 22:17:18 +0300

    Add first step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,70 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        { id: nanoid(), name: this.state.name },
+      ],
+      name: this.state.name,
+    }));
+
+    console.log(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '' });
   };
 
   render() {
     return (
       <div>
         <h2>Phonebook</h2>
-        <form action="">
-          <label htmlFor="">
+        <form onSubmit={this.handleSubmit}>
+          <label>
             Name
-            <input type="text" />
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+              onChange={this.handleNameChange}
+            />
+          </label>
+          <label>
+            <input
+              type="tel"
+              name="number"
+              ppattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
           </label>
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
         <ul>
-          <li></li>
+          {this.state.contacts.map(option => {
+            return <li key={option.id}>{option.name}</li>;
+          })}
         </ul>
       </div>
     );
